6e553818d973cbc798e69aed04897da7776848a7
BlackFallps <kauabrgamer123@gmail.com>
2026-06-23 01:32:13 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,13 +33,13 @@ async def lembrete_fatura():
         cor_vermelho_escuro = discord.Color.from_rgb(139, 0, 0)
         
         embed = discord.Embed(
-            # Título com emojis e negrito para maior destaque
-            title=" **📢 Lembrete: Fatura Semanal** ",
-            # Adicionei \n\n extra aqui para criar o espaçamento que você quer
-            description="**Lembre-se da Fatura Semanal da Fazenda!**\n\nProcure um Gerente ou Dono no Condado para Efetuar o Pagamento e Manter tudo Acertado com a Fazenda \n\n ",
+            title=" **LEMBRETE: FATURA SEMANAL DA FAZENDA** ",
+            # Aqui estão os três \n para criar o espaçamento que você marcou em verde:
+            description="**Lembre-se da fatura semanal da Fazenda!**\n\n\nProcure um Gerente ou Dono no Condado para Efetuar o Pagamento e Manter tudo Acertado com a Fazenda 🤠",
             color=cor_vermelho_escuro
         )
-        # Adiciona um rodapé profissional
+        
+        # O set_footer já mantém o espaçamento padrão lá embaixo
         embed.set_footer(text="©︎ Fazenda Gomes Girardi - Administração")
         await canal.send(embed=embed)
 
